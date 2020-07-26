1ce1a717305053c9bce746dc331338e8b95d809d
podatkar <68809929+podatkar@users.noreply.github.com>
2020-07-26 12:49:23 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("KARMEN")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("AJDA")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("LARA")
+})
+basic.showString("HI")
+basic.forever(function () {
+	
+})
